5a1e9103f156995e019cea8deafa1ed61050d6a8
Mitrofanov Sergey <mitrofanov@f-technology.ru>
2016-03-28 10:46:28 +0300

    Init menu

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -101,4 +101,18 @@ return array(
             ),
         ),
     ),
+	'doctrine' => array(
+		'driver' => array(
+			'App_Entities' => array(
+				'class' =>'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+				'cache' => 'array',
+				'paths' => array(__DIR__ . '/../src/Application/Entity')
+			),
+			'orm_default' => array(
+				'drivers' => array(
+					'Application\Entity' => 'App_Entities'
+				),
+			),
+		),
+	),
 );
