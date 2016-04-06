1bbb6eccfbc200856c510dc733f6736757b3f107
Mitrofanov Sergey <mitrofanov@f-technology.ru>
2016-04-06 11:45:28 +0300

    #Init

---
+++ module/Auth/config/module.config.php
@@ -0,0 +1,86 @@
+<?php
+
+return array(
+    
+    'doctrine' => array(
+        'driver' => array(
+            'Auth_Entities' => array(
+                'class' =>'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => array(__DIR__ . '/../src/Auth/Entity')
+            ),
+            'orm_default' => array(
+                'drivers' => array(
+                    'Auth\Entity' => 'Auth_Entities'
+                ),
+            ),
+        ),
+        
+        'authentication' => array(
+            'orm_default' => array(
+                'object_manager' => 'Doctrine\ORM\EntityManager',
+                'identity_class' => 'Auth\Entity\User',
+                'identity_property' => 'email',
+                'credential_property' => 'password',
+                'credential_callable' => function(\Auth\Entity\User $user, $passwordGiven) {
+                    // using Bcrypt 
+                    $bcrypt   = new \Zend\Crypt\Password\Bcrypt();
+                    $bcrypt->setSalt('m3s3Cr3tS4lty34h');
+
+                    // $passwordGiven is unhashed password that inputted by user
+                    // $user->getPassword() is hashed password that saved in db
+                    return $bcrypt->verify($passwordGiven, $user->getPassword());
+                },
+            ),
+        ),
+    ),
+    
+    'doctrine_factories' => array(
+        'authenticationadapter' => 'Auth\Factory\Authentication\AdapterFactory',
+    ),
+    
+    'service_manager' => array(
+        'factories' => array(
+            'Zend\Authentication\AuthenticationService' => function($serviceManager) {
+                return $serviceManager->get('doctrine.authenticationservice.orm_default');
+            }
+        )  
+    ),
+    
+    'controllers' => array(
+        'factories' => array(
+            'Auth\Controller\Auth' => function($controller) {
+                $authController = new \Auth\Controller\AuthController($controller->getServiceLocator()->get('Zend\Authentication\AuthenticationService'));
+                return $authController;
+            },
+        ),
+    ),
+    
+    
+    'router' => array(
+        'routes' => array(
+            'auth' => array(
+                'type'    => 'Literal',
+                'options' => array(
+                    'route'    => '/auth',
+                    'defaults' => array(
+                        '__NAMESPACE__' => 'Auth\Controller',
+                        'controller'    => 'Auth',
+                        'action'        => 'login',
+                    ),
+                ),
+            ),
+	        'test' => array(
+		        'type'    => 'Literal',
+		        'options' => array(
+			        'route'    => '/test',
+			        'defaults' => array(
+				        '__NAMESPACE__' => 'Auth\Controller',
+				        'controller'    => 'Auth',
+				        'action'        => 'test',
+			        ),
+		        ),
+	        ),
+        ),
+    ),
+);
---
+++ module/Auth/src/Auth/Controller/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Auth\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\Authentication\AuthenticationService;
+
+class AuthController extends AbstractActionController
+{
+    protected $authService;
+   
+    public function __construct(AuthenticationService $authService)
+    {
+        $this->authService = $authService;
+    }
+
+	public function testAction()
+	{
+		$authService = $this->getServiceLocator()->get('Zend\Authentication\AuthenticationService');
+
+		\Zend\Debug\Debug::dump($authService->hasIdentity());
+		die('test');
+	}
+
+    public function loginAction()
+    {
+        //you can grab this data from the Form in real life app
+        $data['login'] = 'admin@admin.com'; 
+        $data['password'] = 'admin';
+        
+        $authService = $this->getServiceLocator()->get('Zend\Authentication\AuthenticationService');
+
+        $adapter = $authService->getAdapter();
+        $adapter->setIdentityValue($data['login']);
+        $adapter->setCredentialValue($data['password']);
+        $authResult = $authService->authenticate();
+
+
+
+
+
+
+    
+        if ($authResult->isValid())
+        {
+            echo 'login succeded';
+        }
+        else
+        {
+            echo 'login failed';
+        }
+        
+        die;
+
+    }
+}
