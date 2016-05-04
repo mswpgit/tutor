4839f649b03de1a97d74702d669103f9db7a00db
Mitrofanov Sergey <mitrofanov@f-technology.ru>
2016-05-04 17:01:55 +0300

    #Init Back

--- module/Back/src/Back/Controller/MaterialController.php
+++ module/Back/src/Back/Controller/MaterialController.php
@@ -13,39 +13,6 @@ class MaterialController extends AbstractActionController
 		$materialService = $this->getServiceLocator()->get('materialService');
 
 		$materialList = $materialService->getMaterialAll();
-		if ($materialService->getMaterialAll())
-		{
-			/** @var $material \Content\Entity\Material */
-			foreach ($materialService->getMaterialAll() as $material)
-			{
-				\Zend\Debug\Debug::dump($material->getTitle());
-				/** @var $category \Content\Entity\Category */
-				$category = $material->getCategory();
-				\Zend\Debug\Debug::dump($category->getTitle());
-			}
-		}
-
-		/** @var $categoryService \Content\Service\CategoryService */
-		$categoryService = $this->getServiceLocator()->get('categoryService');
-
-		if ($categoryService->getCategoryAll())
-		{
-			/** @var $category \Content\Entity\Category */
-			foreach ($categoryService->getCategoryAll() as $category)
-			{
-				\Zend\Debug\Debug::dump($category->getTitle());
-
-				$material = $category->getMaterial();
-				if ($material)
-				{
-					/** @var $mat \Content\Entity\Material */
-					foreach ($material as $mat)
-					{
-						\Zend\Debug\Debug::dump($mat->getTitle());
-					}
-				}
-			}
-		}
 
 		return new ViewModel(
 			array(
